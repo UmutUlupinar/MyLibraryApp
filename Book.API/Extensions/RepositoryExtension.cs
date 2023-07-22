0f33c80b8b9e3cee36e29fc748a53d1bfeabd34a
umutulupinar <umutulupinarrr@gmail.com>
2023-07-22 17:52:55 +0300

    service lifetime extensions are added

---
+++ Book.API/Extensions/RepositoryExtension.cs
@@ -0,0 +1,14 @@
+﻿using Book.Access.Repository.Abstract;
+using Book.Access.Repository.Concrete;
+
+namespace Book.API.Extensions;
+
+public static class RepositoryExtension
+{
+    public static void AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped(typeof(IBookRepository),typeof(BookRepository));
+        services.AddScoped(typeof(ICheckoutsRepository),typeof(CheckoutsRepository));
+        services.AddScoped(typeof(IUserRepository),typeof(UserRepository));
+    }
+}
---
+++ Book.API/Extensions/ServicesExtension.cs
@@ -0,0 +1,13 @@
+﻿using Book.Service.Book;
+using Book.Service.Checkouts;
+
+namespace Book.API.Extensions;
+
+public static class ServicesExtension
+{
+    public static void AddServices(this IServiceCollection services)
+    {
+        services.AddScoped<IBookService,BookService>();
+        services.AddScoped<ICheckoutsService, CheckoutsService>();
+    }
+}
