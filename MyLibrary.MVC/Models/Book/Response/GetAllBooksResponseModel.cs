187216a194add3029010537533517c99a03fa0a2
umutulupinar <umutulupinarrr@gmail.com>
2023-07-23 00:22:21 +0300

    response model is added for getallbooks action

---
+++ MyLibrary.MVC/Models/Book/Response/GetAllBooksResponseModel.cs
@@ -0,0 +1,9 @@
+﻿using MyLibrary.MVC.Models.GenericModels;
+
+namespace MyLibrary.MVC.Models.Book.Response;
+
+public class GetAllBooksResponseModel
+{
+    public string Name { get; set; }
+    public CategoryType CategoryType { get; set; }
+}
