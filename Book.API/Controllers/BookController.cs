4e4e555709b9e401fd95d5e2a181a558e8b4f859
umutulupinar <umutulupinarrr@gmail.com>
2023-07-22 16:11:24 +0300

    basic book request is added

---
+++ Book.API/Controllers/BookController.cs
@@ -0,0 +1,19 @@
+﻿using Book.Service.Book;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Book.API.Controllers;
+
+public class BookController : Controller
+{
+    private readonly IBookService _bookService;
+
+    public BookController(IBookService bookService)
+    {
+        _bookService = bookService;
+    }
+    
+    [HttpGet]
+    public async Task<List<Data.Entities.Book>> GetAllBooks()=> await _bookService.GetAllBooks();
+    
+    
+}
--- Book.Service/Book/BookService.cs
+++ Book.Service/Book/BookService.cs
@@ -1,6 +1,16 @@
-﻿namespace Book.Service.Book;
+﻿using Book.Access.Repository.Abstract;
 
-public class BookService
+namespace Book.Service.Book;
+
+public class BookService : IBookService
 {
-    
-}
+    private readonly IBookRepository _bookRepository;
+
+    public BookService(IBookRepository bookRepository)
+    {
+        _bookRepository = bookRepository;
+    }
+
+    public async Task<List<Data.Entities.Book>> GetAllBooks() =>
+        _bookRepository.Where(b => b.IsDeleted == false).Result.ToList();
+    }
