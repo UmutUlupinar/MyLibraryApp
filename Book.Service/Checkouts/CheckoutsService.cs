64255baa77721740aeffbc197a5ae027c4699f45
umutulupinar <umutulupinarrr@gmail.com>
2023-07-24 17:14:50 +0300

    Checkout service methods initial

--- Book.Service/Checkouts/CheckoutsService.cs
+++ Book.Service/Checkouts/CheckoutsService.cs
@@ -1,6 +1,32 @@
-﻿namespace Book.Service.Checkouts;
+﻿using Book.API.Models.Book;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Book.Service.Checkouts;
 
 public class CheckoutsService:ICheckoutsService
 {
-    
+    public Task<List<Data.Entities.Checkouts>> GetAllCheckouts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Data.Entities.Checkouts>> GetUserCheckouts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<EntityEntry<Data.Entities.Checkouts>> AddCheckout(Data.Entities.Book book)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteCheckout(Guid Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Data.Entities.Checkouts> UpdateCheckout(UpdateBookRequestModel book)
+    {
+        throw new NotImplementedException();
+    }
 }
--- Book.Service/Checkouts/ICheckoutsService.cs
+++ Book.Service/Checkouts/ICheckoutsService.cs
@@ -1,6 +1,13 @@
-﻿namespace Book.Service.Checkouts;
+﻿using Book.API.Models.Book;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Book.Service.Checkouts;
 
 public interface ICheckoutsService
 {
-    
+    Task<List<Data.Entities.Checkouts>> GetAllCheckouts();
+    Task<List<Data.Entities.Checkouts>> GetUserCheckouts();
+    Task<EntityEntry<Data.Entities.Checkouts>> AddCheckout(Data.Entities.Book book);
+    void DeleteCheckout(Guid Id);
+    Task<Data.Entities.Checkouts> UpdateCheckout(UpdateBookRequestModel book);
 }
